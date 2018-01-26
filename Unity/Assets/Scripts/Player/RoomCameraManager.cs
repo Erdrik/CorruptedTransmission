964dd20b76ab08e184c4c7cc1d93aaa81b810c44
Joshua Stevenson <JoshuaStevenson97@gmail.com>
2018-01-26 21:25:14 +0000

    NIGGAS DON'T KNOW SHIT ABOUT MY FLY ASS CAMERA UI DAWGGGGGGGGGGGGGGGGGGG

--- Unity/Assets/Scripts/Player/RoomCameraManager.cs
+++ Unity/Assets/Scripts/Player/RoomCameraManager.cs
@@ -8,6 +8,8 @@ public class RoomCameraManager : MonoBehaviour {
     
     public int _currentCamera;
 
+    public CameraObject[] _cameras;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -32,6 +34,7 @@ public class RoomCameraManager : MonoBehaviour {
     }
 
     public void CycleCameraForward() {
+<<<<<<< refs/remotes/origin/master:Unity/Assets/Scripts/Player/RoomCameraManager.cs
         if (_currentCamera + 1 < _cameras.Count) {
             _currentCamera = _currentCamera + 1;
         }
@@ -47,5 +50,12 @@ public class RoomCameraManager : MonoBehaviour {
         else {
             _currentCamera = _cameras.Count - 1;
         }
+=======
+        Debug.Log("Camera Cycled Forwards");
+    }
+
+    public void CycleCameraBackward() {
+        Debug.Log("Camera Cycled Backwards");
+>>>>>>> NIGGAS DON'T KNOW SHIT ABOUT MY FLY ASS CAMERA UI DAWGGGGGGGGGGGGGGGGGGG:Unity/Assets/Scripts/Player/CameraManager.cs
     }
 }
---
+++ Unity/Assets/Scripts/UI/CommandsManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommandsManager : MonoBehaviour {
+
+    public void MoveFunction(int i)
+    {
+        Debug.Log((Direction)i);
+
+    }
+	
+
+}
