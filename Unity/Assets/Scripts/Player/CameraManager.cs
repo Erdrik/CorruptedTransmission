b3956910e976fc8be41682afd49e02e1f16697f7
Samuel Ballard-Adams <s.ballard-adams@outlook.com>
2018-01-26 18:30:53 +0000

    Starting out

---
+++ Unity/Assets/Scripts/Player/CameraManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void CycleCamera(int cameraIndex) {
+
+    }
+
+    public void CycleCameraForward() {
+
+    }
+
+    public void CycleCameraBackward() {
+
+    }
+}
