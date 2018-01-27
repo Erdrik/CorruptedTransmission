38a4d6c8cb38c74218d039c4e1fab70b4d8321fb
Joshua Stevenson <JoshuaStevenson97@gmail.com>
2018-01-27 13:22:06 +0000

    Auto stash before merge of "Experiment" and "origin/master"

--- Unity/Assets/Scripts/Player/RoomCameraManager.cs
+++ Unity/Assets/Scripts/Player/RoomCameraManager.cs
@@ -11,20 +11,33 @@ public class RoomCameraManager : MonoBehaviour {
     private static Camera _mainStaticCamera;
     private static Room _activeRoom;
 
+    public static RoomCameraManager _instance;
+    public Room _defaultRoom;
+    private void Awake()
+    {
+        _activeRoom = null;
+        _instance = this;
+    }
+
     public void Start()
     {
-        _mainStaticCamera = _mainCamera;
+        _mainStaticCamera = _mainCamera;
+        GoToRoom(_defaultRoom);
     }
 
     public static void GoToRoom(Room room)
     {
-        _mainStaticCamera.transform.SetParent(room._cameraPoints,false);
+        if (room._cameraPoints.Count > 0)
+        {
+            GoToCamera(room._cameraPoints[0]);
+        }
     }
     
     public static void GoToCamera(CameraController camera)
     {
         if(_activeRoom != camera._owningRoom)
         {
+            Debug.Log("check");
             _instance._cameraUIManager.SetActiveRoom(camera._owningRoom);
             _activeRoom = camera._owningRoom;
         }
@@ -32,25 +45,5 @@ public class RoomCameraManager : MonoBehaviour {
 
     }
 
-}
-
-public class RoomCameraManager : MonoBehaviour {
-
-    public static RoomCameraManager _instance;
-
-    public Room _defaultRoom;
-    private void Awake()
-    {
-        _instance = this;
-    }
-
-    public void Start()
-    {
-        _mainStaticCamera = _mainCamera;
-        GoToRoom(_defaultRoom);
-    public static void GoToRoom(Room room)
-    {
-        if(room._cameraPoints.Count > 0)
-        {
-            GoToCamera(room._cameraPoints[0]);
-        }
+}
+        
