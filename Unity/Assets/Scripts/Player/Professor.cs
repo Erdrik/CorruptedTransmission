6d014c41b4a717b8ce954543df16695d3343cd19
Samuel Ballard-Adams <s.ballard-adams@outlook.com>
2018-01-27 02:04:08 +0000

    Professor and MakeNice come to life
    The Professor and MakeNice will work together to manuever the game! The Professor is a RoomWalker. Put them together and we work well!

---
+++ Unity/Assets/Scripts/Player/Professor.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Professor : MonoBehaviour {
+
+    private Dictionary<string, Room> _knownRooms;
+
+    public RoomWalker _roomWalker;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void TeachRoom(string tag, Room room) {
+        _knownRooms.Add(tag, room);
+    }
+
+    public void GoToRoom(Room room) {
+        if (_knownRooms.ContainsKey(room._tag)) {
+            _roomWalker.EnterRoom(_knownRooms[name]);
+        }
+        else {
+            Complain();
+        }
+    }
+
+    public void GetOut() {
+        _roomWalker.EnterRoom(_roomWalker._currentRoom.GetRandomNeighbourRoom());
+    }
+
+    public void GoBack() {
+        _roomWalker.EnterPreviousRoom();
+    }
+
+    public void Stop() {
+        _roomWalker.Stop();
+    }
+
+    public void Hide() {
+        _roomWalker.MoveTowards(_roomWalker._currentRoom.GetRandomHidingPoint());
+    }
+
+    public void PushButton() {
+        //_roomWalker.MoveTowards(_roomWalker._currentRoom.GetPushButtonPoint());
+    }
+
+    private void Complain() {
+        Debug.Log("The professor complains about something!");
+    }
+}
